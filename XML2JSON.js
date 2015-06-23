9ceb5921e705f51263cb45b06223ed7eef3247bf
Peabnuts123 <peabnuts123@gmail.com>
2015-06-22 22:31:23 -0700

    Added test running and displaying of result tables

--- XML2JSON.js
+++ XML2JSON.js
@@ -4,6 +4,7 @@
 //NodeJS
 var fs = require('fs');
 var _ = require('underscore');
+var _str = require('underscore.string');
 
 //Constants
 var XML_FILE_NAME = "data.xml";
@@ -13,11 +14,17 @@ var JSON_FILE_NAME = "data.json";
 var XMLData = getFileContents(XML_FILE_NAME);
 var JSONData = getFileContents(JSON_FILE_NAME);
 
-//Execution
-runTests();
-
-
 //REGISTER TEST FUNCTIONS
+//Variables
+var convertJSONToXML = [];
+var convertXMLToJSON = [];
+
+
+
+//==============================
+// <THE BIT YOU NEED TO ADD TO>
+//==============================
+
 //JSON to XML
 convertJSONToXML.push(
 	new convertFunction(
@@ -36,6 +43,7 @@ convertJSONToXML.push(
 			return XMLData;
 		}
 	)
+	//Add more convertFunction objects here
 );
 
 //XML to JSON
@@ -53,21 +61,86 @@ convertXMLToJSON.push(
 			return JSONData;
 		}
 	)
+	//Add more convertFunction objects here
 );
 
+//===============================
+// </THE BIT YOU NEED TO ADD TO>
+//===============================
+
+
+
+//EXECUTION
+runTests();
 
 //UTILITY
 //Run tests
 function runTests() {
-	console.log(" === XML2JSON === ")
-	console.log("")
-	console.log(" == Total Functions: " + convertXMLToJSON.length + " == ");
+	runTest("JSON2XML", convertJSONToXML, JSONData, XMLData);
+	runTest("XML2JSON", convertXMLToJSON, XMLData, JSONData);
+}
+
+function runTest(name, cfCollection, testData, expectedResult) {
+	var formatWidth = 50;
+
+	var tableDelimeter = " | ";
+
+	var columnWidths = {
+		Name : Math.ceil(0.6 * (formatWidth - 4 - (tableDelimeter.length * 2))),
+		Result : Math.floor(0.2 * (formatWidth - 4 - (tableDelimeter.length * 2)))
+	};
+	columnWidths.Score =  formatWidth - 4 - columnWidths.Name - columnWidths.Result - (tableDelimeter.length * 2);
+
+	console.log("  ==" + centeredString(name, formatWidth - 8) + "==  ");
+	console.log("  ==" + centeredString("Total Functions: " + cfCollection.length, formatWidth - 8) + "==  ");
+
+	var tableFormatString = "  %-" + columnWidths.Name + "s" + tableDelimeter + "%" + columnWidths.Result + "s" + tableDelimeter + "%" + columnWidths.Score + "s  ";
+	console.log(_str.sprintf(tableFormatString, "Name", "Result", "Score"));
+	console.log('  ' + new Array(formatWidth + 1 - 4).join('=') + '  ');
+
+	var results = _.map(cfCollection, function(cf) {
+		return {
+			name: cf.name,
+			result: cf.f(testData) === expectedResult,
+			score: cf.score()
+		};
+	});
+
+	var sortedResults = _.sortBy(results, function(cf) {
+		if (cf.result) {
+			return cf.score;
+		} else {
+			return 100000 + cf.score;
+		}
+	});
+
+	_.each(sortedResults, function(cf) {
+		var result = cf.result;
+		var resultText = result ? "Passed" : "Failed";
+
+		var scoreText = "" + cf.score;
+
+		console.log(_str.sprintf("  %-" + columnWidths.Name + "s" + tableDelimeter + "%" + columnWidths.Result + "s" + tableDelimeter + "%" + columnWidths.Score + "s", cf.name, resultText, scoreText));
+	});
+
+	console.log("");
+}
+
+//Create a string that is centered in a string of a certain width
+function centeredString(s, width) {
+	var centeringSpaceLeft = (Math.ceil((width - s.length - 2)/2));
+	var centeringSpaceRight = (Math.floor((width - s.length - 2)/2));
+
+	return _str.sprintf("%s %s %s", new Array(centeringSpaceLeft+1).join('-'), s, new Array(centeringSpaceRight+1).join('-'));
 }
 
 //Create object that holds a name and a function
 function convertFunction(name, f) {
 	this.name = name;
 	this.f = f;
+	this.score = function() {
+		return this.f.toString().replace(/^[^{]*{([\s\S]*)}.*$/gm, "$1").replace(/\s{2,}/gm," ").trim().length;
+	}
 }
 
 //Helper to read data from file
